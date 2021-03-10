a547375633a350b041c803c995ed0bfb161e198b
cbwces <sknyqbcbw@gmail.com>
2021-03-11 03:14:05 +0800

    modify image predict stage script

--- image_predict.py
+++ image_predict.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 import torch
@@ -6,7 +7,7 @@ from torch.nn import functional as F
 import numpy as np
 from torch.utils.data import DataLoader
 from image_utils import AgeData
-from efficientnet_pytorch import EfficientNet
+from network_utils import StnModule, MainModel
 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = True
@@ -28,20 +29,18 @@ for line in f.read().strip().split('\n'):
     test_label.append(-1)
 test_pair = (test_line, test_label)
 f.close()
-test_set = AgeData(test_pair, is_train=False, normal_aug=args['test_preprocess'], test_time_aug=args['test_time_augmentation'], img_size=args['img_size'], num_classes=args['num_classes'], mode=args['tta_mode'], crop_info=args['data_file_info'])
+test_set = AgeData(test_pair, is_train=False, normal_aug=args['test_preprocess'], test_time_aug=args['test_time_augmentation'], img_size=args['img_size'], num_classes=args['num_classes'], mode=args['tta_mode'], crop_info=args['data_file_info'], crop_margin=args['margin'], is_affine=args['affine'])
 
 test_loader = DataLoader(test_set, batch_size=1)
 
 if args['stn'] == True:
-    stem_model = EfficientNet.from_name('efficientnet-b0')
-    stem_model._fc = nn.Linear(stem_model._fc.in_features, args['num_classes']-1)
+    stem_model = MainModel(args['num_classes'])
     model = nn.Sequential(StnModule(img_size=args['img_size']), stem_model)
 else:
-    model = EfficientNet.from_name('efficientnet-b0')
-    model._fc = nn.Linear(model._fc.in_features, args['num_classes']-1)
+    model = MainModel(args['num_classes'])
 
 model = model.to(DEVICE)
-model.load_state_dict(torch.load(args['model']))
+model.load_state_dict(torch.load(os.path.join(args['save_dir'], 'age_model.pth')))
 
 model.eval()
 if args['tta_mode'] >= 2:
@@ -52,12 +51,13 @@ with torch.no_grad():
         if args['tta_mode'] < 2:
             X = X.to(DEVICE).float()
             pred_value = torch.sum(torch.sigmoid(model(X)) > 0.5, dim=1) + 1
+            pred_value = pred_value.to('cpu').numpy()[0]
         else:
             for no_aug_img, aug_X in enumerate(X):
                 aug_X = aug_X.to(DEVICE).float()
                 pred_value_chunk[no_aug_img] = torch.sigmoid(model(aug_X))
             pred_value = torch.sum((torch.sum(pred_value_chunk, dim=0) / args['tta_mode']) > 0.5, dim=0) + 1
-        pred_value = pred_value.to('cpu').numpy()
+            pred_value = pred_value.to('cpu').numpy()
         if args['cuda'] == True:
             torch.cuda.empty_cache()
         print("img: " + test_set.img_paths[i])
