1. fix different backbone choice wrong code 2. guided backpropagation also used on swish activation func

network_utils.py
@@ -60,21 +60,26 @@ class MainModel(nn.Module):
 
     def __init__(self, backbone, num_classes, pretrain=False):
         super(MainModel, self).__init__()
+        self.backbone = backbone
         if backbone == 'E':
             if pretrain == True:
                 self.model = EfficientNet.from_pretrained('efficientnet-b4')
             else:
                 self.model = EfficientNet.from_name('efficientnet-b4')
-            # self.model._fc = nn.Linear(self.model._fc.in_features, num_classes-1, bias=False)
-            self.model._fc = nn.Linear(self.model._fc.in_features, num_classes-1)
-        else if backbone == 'R':
+            self.model._fc = nn.Linear(self.model._fc.in_features, num_classes-1, bias=False)
+            self.last_bias = nn.Parameter(torch.zeros(num_classes-1).float())
+
+        elif backbone == 'R':
             if pretrain == True:
                 self.model = torchvision.models.resnet101(pretrained=True, num_classes=num_classes-1)
             else:
                 self.model = torchvision.models.resnet101(num_classes=num_classes-1)
+        else:
+            raise KeyError
 
     def forward(self, x):
         x = self.model(x)
-        # x = x + self.last_bias
+        if self.backbone == 'E':
+            x = x + self.last_bias
         return x
 

visualize/visualize.py
@@ -9,7 +9,7 @@ from torch import nn as nn
 from torch.nn import functional as F
 import numpy as np
 
-sys.path.append('/home/cbw233/python/bench_test/')
+sys.path.append('/home/cbw233/python/age_estimate/')
 from grad import GuidedBackpropSmoothGrad, save_as_gray_image
 from network_utils import StnModule, MainModel
 from image_utils import *